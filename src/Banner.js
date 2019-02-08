00a59ed79627d3357120183f25be5885dab3c554
Tom Betthauser <tombetthauser@gmail.com>
2019-02-08 14:52:05 -0800

    scale test

--- src/Banner.js
+++ src/Banner.js
@@ -60,6 +60,7 @@ const bannerClass = () => {
 const Banner = (props) => {
 	return (
 		<div>
+			<meta name="viewport" content="width=device-width, initial-scale=0.2, maximum-scale=1.0, user-scalable=no" />
 			<div id='spacer'>
 				{
 					// robots.length + ' resources' + textB[txtNum] + '...'
